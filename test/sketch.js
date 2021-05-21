2e0ad14807bca785dee1a5316c1c3b105456da36
Swivfer49 <78223283+Swivfer49@users.noreply.github.com>
2021-05-21 10:07:49 -0500

    now more understandable
    this adds comments so that you can understand better

--- test/sketch.js
+++ test/sketch.js
@@ -1,26 +1,30 @@
-let selectedTool="";
+let selectedTool="";//this indicates what tool is selected
 
-let papers = [];
+let papers = [];//this is the array with all the sticky papers in it
 
-let drag;
+let drag; //this holds the drag details
 
-function setup(){
-    createCanvas(windowWidth,windowHeight-40);
+
+//these are the main processing function
+function setup(){//this is called at the start
+    createCanvas(windowWidth,windowHeight-40);//this determines the canvas size of the board
     
 
-    for(let i=0;i<10;i++){
+    for(let i=0;i<10;i++){//these are the starting papers
         papers[i]= new paper(120,260,150,150);
 
     }
 
-     drag=new dragDetails();
+     drag=new dragDetails();//this initializes the drag information
 }
-function draw(){
+function draw(){//this is called every frame after the start
   background("#fdeca6");
   for(let i=0;i<papers.length;i++){
       papers[i].draw();
-      
+      //draws all the papers on screen
   }
+
+  //this makes the recycling bin visible
   if(drag.isDragging==true){
       fill(0,0,250);
       rect(width-80,height-80,width,height);
@@ -29,29 +33,31 @@ function draw(){
   
 }
 
-function mousePressed(){
-    if(selectedTool=='draw'){
+
+//these are the mouse events
+function mousePressed(){//this is when mouse starts being pressed
+    if(selectedTool=='draw'){//this draws on the notes
         let d=true;
         papers.reverse();
         for(let i=0;i<papers.length;i++){
             if((ptInsidePaper(createVector(mouseX,mouseY),papers[i])||ptInsidePaper(createVector(pmouseX,pmouseY),papers[i]))&&d==true){
                 d=false;
-                if(mouseButton===LEFT){
+                if(mouseButton===LEFT){//the draw tool
                     papers[i].display.stroke(0);
                     papers[i].display.strokeWeight(3);
-                }else{
+                }else{//the erase tool
                     papers[i].display.stroke(255,247,64);
                     papers[i].display.strokeWeight(13);
                 }
                 let px=papers[i].x;
                 let py=papers[i].y;
-                papers[i].display.line(mouseX-px,mouseY-py,pmouseX-px,pmouseY-py);
+                papers[i].display.line(mouseX-px,mouseY-py,pmouseX-px,pmouseY-py);//this is a line on the paper
             }
         }
         papers.reverse();
     }
     
-    if(drag.isDragging==false&&selectedTool=='move'){
+    if(drag.isDragging==false&&selectedTool=='move'){//this starts dragging the top paper that overlaps the mouse pointer
         papers.reverse();
         let newPapersList=[];
         let gotOne=false;
@@ -72,11 +78,11 @@ function mousePressed(){
         papers.reverse();
     }
 }
-function mouseDragged(){
+function mouseDragged(){//this is when the mouse is down and gets moved
     if(drag.isDragging==true){
         drag.durringDragging();
     }
-    if(selectedTool=='draw'){
+    if(selectedTool=='draw'){//this also does drawing on the papers
         let d=true;
         papers.reverse();
         for(let i=0;i<papers.length;i++){
@@ -97,21 +103,23 @@ function mouseDragged(){
         papers.reverse();
     }
 }
-function mouseReleased(){
-    if(drag.isDragging==true){
+function mouseReleased(){//this is when the mouse is released
+    if(drag.isDragging==true){//this ends the dragging
         drag.endDragging();
         if(mouseX>width-80&&mouseY>height-80){
-           
+           //this deletes the dragged paper if mouse is in the recycle bin
             papers.pop();
             
         }
     }
 }
 
-function doToolbar(){
 
+//these are the toolbar things
+function doToolbar(){//this is called after the tool changes
 
-//xander
+//this disables or enables specific buttons
+
     if(selectedTool=="move"){
         document.getElementById("move-tool").disabled = true;
         styleButtonSelected("move-tool");
@@ -134,12 +142,12 @@ function doToolbar(){
 
 
 }
-function setTool(toolName){
+function setTool(toolName){//this allows the html buttons to set the tool
     selectedTool=toolName;
     doToolbar();
 }
 
-function createPaper(){
+function createPaper(){//this creates a paper where the mouse is
     if(drag.isDragging==false){
         let newNote=new paper(mouseX,mouseY,150,150);
         drag.startDragging(newNote);
@@ -147,14 +155,18 @@ function createPaper(){
     }
 }
 
-function styleButtonNonSelected(buttonId){
+
+//these style buttons
+function styleButtonNonSelected(buttonId){//this styles the buttons to look not selected
     document.getElementById(buttonId).style = "border-width: 1px; border-style: outset; border-color: -internal-light-dark(rgb(118, 118, 118), rgb(133, 133, 133)); border-radius: 4px;";
 }
-function styleButtonSelected(buttonId){
+function styleButtonSelected(buttonId){//this styles th buttons to look selected
     document.getElementById(buttonId).style = "border: 1px solid black; border-radius: 4px;";
 }
 
-class dragDetails{
+
+//these are the classes
+class dragDetails{//this is a class that holds drag information
 
     constructor(){
         this.isDragging=false;
@@ -182,8 +194,7 @@ class dragDetails{
     }
 
 }
-
-class paper{
+class paper{//this is a class that is a paper
 
     constructor(x,y,w,h){
         this.x=x;
@@ -191,11 +202,11 @@ class paper{
         this.width=w;
         this.height=h;
         this.rot=0;
-        this.display= createGraphics(this.width,this.height);
+        this.display= createGraphics(this.width,this.height);//this is the image that the paper displays
         this.display.background(255,247,64);
     }
 
-    draw(){
+    draw(){//this draws the paper on screen
         fill(240,236,55);
         stroke("#ffca18")
         rect(this.x-2,this.y-2,this.width+2,this.height+2);
@@ -203,7 +214,10 @@ class paper{
     }
 
 }
-function ptInsidePaper(pt,ppr){
+
+
+//this didn't fit in with the other function so i put it here
+function ptInsidePaper(pt,ppr){//this determines if a paper intersects the mouse
     let ret=(
         pt.x>= ppr.x
         &&pt.x<=ppr.width+ppr.x
